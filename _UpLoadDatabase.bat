f06ec0e54f89fadfbcc71470912b126996d18af1
Vaqif87 <fatiev_va@tehtsk.ru>
2019-08-30 03:18:19 +0700

    фигня какая-то пуш

---
+++ _UpLoadDatabase.bat
@@ -0,0 +1,11 @@
+if not exist _backups mkdir _backups
+set filename="_backups/base_%Date:~6,4%.%Date:~3,2%.%Date:~0,2%_%time:~-11,2%.%time:~-8,2%.%time:~-5,2%.sql"
+set filename=%filename: =%
+echo %filename%
+C:\WebServers\OpenServer\OSPanel\modules\database\MySQL-5.6-x64\bin\mysqldump.exe -uidoctor -pidoctor idoctor > %filename%
+C:\WebServers\OpenServer\OSPanel\modules\database\MySQL-5.6-x64\bin\mysql.exe -uidoctor -pidoctor -e "drop database idoctor"
+C:\WebServers\OpenServer\OSPanel\modules\database\MySQL-5.6-x64\bin\mysql.exe -uidoctor -pidoctor -e "create database idoctor"
+C:\WebServers\OpenServer\OSPanel\modules\database\MySQL-5.6-x64\bin\mysql.exe -uidoctor -pidoctor idoctor < idoctor.sql
+
+
+
